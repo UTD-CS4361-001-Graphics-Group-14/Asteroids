141e065cc8fbaeb8bbed0568f42ebf59d4142b26
Eliot Partridge <byte@code.horse>
2021-10-24 19:03:44 -0500

    Vector2: add rotate(d) and scale(d) methods

--- util/vector2.lua
+++ util/vector2.lua
@@ -9,6 +9,10 @@ function Vector2:new(x, y)
 	return v
 end
 
+function Vector2:newFromMagnitudeAndAngle(magnitude, angle)
+	return Vector2:new(magnitude * math.cos(angle), magnitude * math.sin(angle))
+end
+
 function Vector2:add(v)
 	self.x = self.x + v.x
 	self.y = self.y + v.y
@@ -57,6 +61,30 @@ end
 function Vector2:normalized()
 	local mag = self:magnitude()
 	return Vector2:new(self.x / mag, self.y / mag)
+end
+
+function Vector2:rotate(angle)
+	local x = self.x
+	local y = self.y
+	self.x = x * math.cos(angle) - y * math.sin(angle)
+	self.y = x * math.sin(angle) + y * math.cos(angle)
+end
+
+function Vector2:rotated(angle)
+	local x = self.x
+	local y = self.y
+	return Vector2:new(x * math.cos(angle) - y * math.sin(angle), x * math.sin(angle) + y * math.cos(angle))
+end
+
+function Vector2:scale(magnitude)
+	local mag = self:magnitude()
+	self.x = self.x / mag * magnitude
+	self.y = self.y / mag * magnitude
+end
+
+function Vector2:scaled(magnitude)
+	local mag = self:magnitude()
+	return Vector2:new(self.x / mag * magnitude, self.y / mag * magnitude)
 end
 
 return Vector2
