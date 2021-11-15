7cc283618c68d28ff29c791d64cdf7e4ba0b3fee
Pei-Yun Tseng <peitseng@MacBook-Pro-22.home>
2021-11-14 18:38:28 -0600

    ship remains in window

--- entities/ship.lua
+++ entities/ship.lua
@@ -35,11 +35,31 @@ function Ship:update(dt)
         self.y2 = self.y2 + self.speed * dt
         self.y3 = self.y3 + self.speed * dt    
     end
+
+    -- make sure ship remains in the boundary of the window
+    if self.x < 0 then
+        self.x = 0
+        self.x2 = self.x + 50
+        self.x3 = self.x + 25
+    elseif self.y3 < 0 then
+        self.y3 = 0
+        self.y2 = self.y3 + 70
+        self.y = self.y3 + 70
+    elseif self.x2 > love.graphics.getWidth() then
+        self.x2 = love.graphics.getWidth()
+        self.x = love.graphics.getWidth() - 50
+        self.x3 = love.graphics.getWidth() - 25
+    elseif self.y2 > love.graphics.getHeight() then
+        self.y2= love.graphics.getHeight()
+        self.y = love.graphics.getHeight()
+        self.y3 = love.graphics.getHeight() - 70
+    end
 end
 
 function Ship:draw()
 	love.graphics.setColor(255, 255, 255)
 	love.graphics.polygon('fill', self.x, self.y, self.x2, self.y2, self.x3, self.y3)
 end
+
 
 return Ship
