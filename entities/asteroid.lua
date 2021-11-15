6eaf17417c0e0130601f1d6b57ba0d34591e56b2
Eliot Partridge <byte@code.horse>
2021-11-14 19:18:10 -0600

    fix: Asteroids now wrap off the top and left sides of the screen

--- entities/asteroid.lua
+++ entities/asteroid.lua
@@ -24,10 +24,16 @@ end
 
 function Asteroid:update(dt)
 	self.pos:add(self.vel:product(dt))
-	if self.pos.x > love.graphics.getWidth() then
+
+	if self.pos.x < 0 then
+		self.pos.x = self.pos.x + love.graphics.getWidth()
+	elseif self.pos.x > love.graphics.getWidth() then
 		self.pos.x = self.pos.x - love.graphics.getWidth()
 	end
-	if self.pos.y > love.graphics.getHeight() then
+
+	if self.pos.y < 0 then
+		self.pos.y = self.pos.y + love.graphics.getHeight()
+	elseif self.pos.y > love.graphics.getHeight() then
 		self.pos.y = self.pos.y - love.graphics.getHeight()
 	end
 end
