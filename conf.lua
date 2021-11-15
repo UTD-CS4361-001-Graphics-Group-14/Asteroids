30e8036baaa84f9f007b8bf6b86374bcbe6b8818
Eliot Partridge <byte@code.horse>
2021-11-14 20:44:40 -0600

    More specific identifier

--- conf.lua
+++ conf.lua
@@ -1,8 +1,10 @@
 function love.conf(t)
 	t.version = '11.3'
-	t.identity = 'asteroids'
+	t.identity = 'cs4361.001f21-g14-asteroids'
 
 	t.console = true -- TODO: change to false for release!
 
 	t.window.title = 'Asteroids!'
+
+	t.window.highdpi = true
 end
